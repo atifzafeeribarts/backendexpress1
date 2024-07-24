f59fa4772cce68338240529d0e6fda5e9b882bff
atifzafeeribarts <atifzafeer@ibarts.in>
2024-07-25 04:27:45 +0530

    second COmmit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs');
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const file_directory = path.join(__dirname, 'files/');
 
